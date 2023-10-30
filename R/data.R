bc8be30d880ff75a7627325b687a8ba8dd12ba2d
Katarina Luise Matthes <kmatth@iem.uzh.ch>
2023-10-30 17:21:28 +0100

    groups added

--- R/data.R
+++ R/data.R
@@ -1,18 +1,85 @@
-data_birth <- read.xlsx("data/birth_num.xlsx",detectDates = TRUE) %>%
-  mutate(Year = year(birth),
+data_birth_g <- read.xlsx("data/birth_count_per_subgroup_1987_2022.xlsx",detectDates = TRUE) %>%
+  mutate(birth = paste0(birth_Y_M,"-01"),
+         birth =ymd(birth),
+         Year = year(birth),
          Year = as.factor(Year),
-         Month = month(birth))
+         Month = month(birth)) %>%
+  rename(total_birth=n) %>%
+  select(-birth_Y_M)
+
+
+data_birth_1871 <- read.xlsx("data/FSO_published_livebriths_per_months_1871_2023.xlsx",detectDates = TRUE) %>%
+  mutate(birth = ymd(paste0(Year,"-", Month,"-01")),
+         Year = as.factor(Year)) %>%
+  select(-Count_IEM)
+
+
+data_birth <- bind_rows(data_birth_g, data_birth_1871)
 
 data_pop_total <- read.xlsx("data/population.xlsx") %>%
   gather(.,Year, population, `1860`:`2022`) %>%
-  filter(Year > 1986) %>%
+  filter(Year > 1870) %>%
   filter(Alter == "Alter - Total") %>%
   filter(Geschlecht=="Geschlecht - Total")
 
 
+data_pop_citizen <- read.xlsx("data/Population_1987_2022.xlsx") %>%
+  rename(population.citizen = `Population.on.31.December`) %>%
+  rename(Geschlecht = Sex) %>%
+  rename(Alter = Age) %>%
+  mutate(Year=as.character(Year)) %>%
+  filter(Canton =="Switzerland") %>%
+  filter(Alter == "Age - total") %>%
+  filter(Geschlecht=="Sex - total") %>%
+  filter(!`Citizenship.(category)` == "Citizenship (category) - total")
+
+
+data_pop_language <- read.xlsx("data/Population_1987_2022.xlsx") %>%
+  rename(population = `Population.on.31.December`) %>%
+  rename(Geschlecht = Sex) %>%
+  rename(Alter = Age) %>%
+  mutate(Year=as.character(Year)) %>%
+  filter(!Canton =="Switzerland") %>%
+  filter(Alter == "Age - total") %>%
+  filter(Geschlecht=="Sex - total") %>%
+  filter(`Citizenship.(category)` == "Citizenship (category) - total") %>%
+  mutate(Language = Canton,
+         Language = recode(Language,
+                           "Zürich" = "German",
+                           "Bern / Berne" = "German",
+                           "Luzern" ="German",
+                           "Uri" = "German",
+                           "Schwyz" = "German",
+                           "Obwalden" = "German",
+                           "Nidwalden" = "German",
+                           "Glarus" = "German",
+                           "Zug" = "German",
+                           "Solothurn" = "German",
+                           "Basel-Stadt" = "German",
+                           "Basel-Landschaft" = "German",
+                           "Schaffhausen" = "German",
+                           "Appenzell Ausserrhoden" = "German",
+                           "Appenzell Innerrhoden" = "German",
+                           "St. Gallen" = "German",
+                           "Graubünden / Grigioni / Grischun" = "German",
+                           "Aargau" = "German",
+                           "Thurgau" = "German",
+                           "Ticino" = "Italian",
+                           "Fribourg / Freiburg" ="French",
+                           "Valais / Wallis" ="French",
+                           "Vaud" ="French",
+                           "Neuchâtel" ="French",
+                           "Genève" ="French",
+                           "Jura" ="French")) %>%
+  filter(!Language == "No indication") %>%
+  group_by(Year,Language) %>%
+  mutate(pop.language = sum(population)) %>%
+  ungroup()
+
+
 data_pop_rep_women <- read.xlsx("data/population.xlsx") %>%
   gather(.,Year, population, `1860`:`2022`) %>%
-  filter(Year > 1986) %>%
+  filter(Year > 1870) %>%
   filter(!Alter == "Alter - Total") %>%
   filter(!Alter == "99 Jahre und mehr") %>%
   filter(Geschlecht=="Frau") %>%
@@ -26,37 +93,39 @@ data_pop_rep_women <- read.xlsx("data/population.xlsx") %>%
   select(Year, Geschlecht,population) %>%
   mutate(Alter="15-49") 
 
-data_tmp_t <- data.frame(Year=rep(seq(1987,2022,1),11), population=rep(NA,396),Month=rep(seq(2,12,1),36),
-                         Alter=rep("Alter - Total", 396), Geschlecht=rep("Geschlecht - Total", 396)) %>%
-  mutate(Year = as.factor(Year))
+# data_tmp_t <- data.frame(Year=rep(seq(1871,2022,1),11), population=rep(NA,1672),Month=rep(seq(2,12,1),152),
+#                          Alter=rep("Alter - Total", 1672), Geschlecht=rep("Geschlecht - Total", 1672)) %>%
+#   mutate(Year = as.factor(Year))
+# 
+# 
+# data_tmp_w <- data.frame(Year=rep(seq(1871,2022,1),11), population=rep(NA,1672),Month=rep(seq(2,12,1),152),
+#                          Alter=rep("15-49", 1672), Geschlecht=rep("Frau", 1672)) %>%
+#   mutate(Year = as.factor(Year))
+
+pop_month_total <- data_pop_total 
+# %>%
+#   mutate(Month=1) 
+  # full_join(data_tmp_t) %>%
+  # arrange(Year, Month) %>%
+  # mutate(pop.monthly = round(zoo::na.approx(population, na.rm=FALSE),0),
+  #        pop.monthly = ifelse(Year==2022 ,8815385, pop.monthly))
 
 
-data_tmp_w <- data.frame(Year=rep(seq(1987,2022,1),11), population=rep(NA,396),Month=rep(seq(2,12,1),36),
-                         Alter=rep("15-49", 396), Geschlecht=rep("Frau", 396)) %>%
-  mutate(Year = as.factor(Year))
-
-pop_month_total <- data_pop_total %>%
-  mutate(Month=1) %>%
-  full_join(data_tmp_t) %>%
-  arrange(Year, Month) %>%
-  mutate(pop.monthly = round(zoo::na.approx(population, na.rm=FALSE),0),
-         pop.monthly = ifelse(Year==2022 ,8815385, pop.monthly))
-
-
-pop_month_women <- data_pop_rep_women %>%
-  mutate(Month=1) %>%
-  full_join(data_tmp_w) %>%
-  arrange(Year, Month) %>%
-  mutate(pop.monthly = round(zoo::na.approx(population, na.rm=FALSE),0),
-         pop.monthly = ifelse(Year==2022 ,1936043, pop.monthly))
+pop_month_women <- data_pop_rep_women 
+# %>%
+#   mutate(Month=1) 
+  # full_join(data_tmp_w) %>%
+  # arrange(Year, Month) %>%
+  # mutate(pop.monthly = round(zoo::na.approx(population, na.rm=FALSE),0),
+  #        pop.monthly = ifelse(Year==2022 ,1936043, pop.monthly))
 
 data_pop <- rbind(pop_month_total, pop_month_women) %>%
-  mutate(Year=as.factor(Year))
+  mutate(Year=as.factor(Year)) %>%
+  full_join(data_pop_citizen) %>%
+  full_join(data_pop_language)
        
-
-
 data_total <- data_birth %>%
-  left_join(data_pop)
+  full_join(data_pop)
 
 write.xlsx( data_total,"data/data_total.xlsx")
 
--- R/excess_birth_subgroupsl.R
+++ R/excess_birth_subgroupsl.R
@@ -1,13 +1,15 @@
 
-function_inla_total <- function(Year_max, Year_min,Sex) {
+function_inla_total <- function(varBirth,varPop,Year_max, Year_min,pop_group) {
   
   load("data/data_total.RData")
   
   dat.excess <- data_total %>%
-    filter(Geschlecht==Sex) %>%
-    select(birth, Year, Month, count,pop.monthly) %>%
+    filter(Geschlecht==pop_group) %>%
+    select(eval(substitute(varBirth)), birth,Year, Month,eval(substitute(varPop))) %>%
     mutate(Year=as.numeric(as.character(Year)))%>%
-    filter(Year >=Year_min & Year <=Year_max )
+    filter(Year >=Year_min & Year <=Year_max ) %>%
+    rename(birth_var = eval(substitute(varBirth)),
+           denominator = eval(substitute(varPop)))
   
   
   year_smooth <- 5
@@ -26,19 +28,14 @@ function_inla_total <- function(Year_max, Year_min,Sex) {
   # # f(timeID, model='seasonal',season.length=12)
   
 
-  formula <- count ~ 1 + offset(log(pop.monthly))  +
+  formula <- birth_var~ 1 + offset(log(denominator))  +
     timeID +
     as.factor(MonthID2) +
-    # YearID+
-     f(YearID, model='ar1',hyper=hyper.iid) +
-    # f(MonthID, model='ar1',hyper=hyper.iid) +
-    # f(timeID2, model='ar1',hyper=hyper.iid, constr=T) 
-    # f(seasID, model='seasonal', season.length =12)
     f(timeID2, model='rw1',scale.model = T,cyclic = TRUE, hyper=hyper.iid)
   # f(timeID, model='seasonal',season.length=12)
   
   
-  # formula <- count ~ 1 + offset(log(pop.monthly))  + 
+  # formula <- eval(substitute(varBirth)) ~ 1 + offset(log(pop.monthly))  + 
   #   f(MonthID, model='iid',hyper=hyper.iid) +
   #   f(seasID, model='seasonal', season.length =12) +
   #   f(timeID, model='rw1',scale.model = T,cyclic = TRUE, hyper=hyper.iid) 
@@ -65,9 +62,10 @@ function_inla_total <- function(Year_max, Year_min,Sex) {
     # }
     # 
    
+    
       reg_data <-  dat.excess %>% 
-        filter(Year >= YEAR+1 - year_smooth & Year < YEAR+1)%>%
-        mutate(count=ifelse (Year ==YEAR, NA, count)) %>% 
+        filter(Year >= YEAR+1 - year_smooth & Year < YEAR+1) %>%
+        mutate(birth_var =ifelse (Year ==YEAR, NA,birth_var)) %>% 
         # filter(!Year == Year_Pan)  %>% 
         # filter(!Year==1918) %>%
         arrange(Year, Month) %>%
@@ -113,12 +111,12 @@ function_inla_total <- function(Year_max, Year_min,Sex) {
     Data= cbind(reg_data,dM)
     
     mean.samples <- Data %>%
-      select(starts_with("V"), "Month", "Year", "count", "pop.monthly") %>%
+      select(starts_with("V"), "Month", "Year") %>%
       rowwise(Month) %>%
       mutate(fit = median(c_across(V1:V1000)),
              LL = quantile(c_across(V1:V1000), probs= 0.025),
              UL = quantile(c_across(V1:V1000), probs= 0.975)) %>%
-      select(Month, fit, LL, UL, Year, count,pop.monthly) %>%
+      select(Month, fit, LL, UL, Year) %>%
       filter(Year==YEAR) %>%
       arrange(Year, Month) %>%
       left_join(dat.excess, by=c("Year", "Month"))
@@ -136,18 +134,21 @@ function_inla_total <- function(Year_max, Year_min,Sex) {
   expected_birth <- expected_birth %>%
     bind_rows(., .id = "column_label")
   
-  write.xlsx( expected_birth,paste0("data/expected_birth_inla_month_",Sex,".xlsx"), rowNames=FALSE, overwrite = TRUE)
-  save( expected_birth,file=paste0("data/expected_birth_inla_month_",Sex,".RData"))
+  write.xlsx( expected_birth,paste0("data/expected_birth_inla_month_",varBirth,"_",pop_group,".xlsx"), rowNames=FALSE, overwrite = TRUE)
+  save( expected_birth,file=paste0("data/expected_birth_inla_month_",varBirth,"_",pop_group,".RData"))
 }
 
+function_inla_total(varBirth="parity_1",varPop="total_birth",Year_max=2022, Year_min=2005,pop_group="Geschlecht - Total")
+function_inla_total(varBirth="parity_sup_1",varPop="total_birth",Year_max=2022, Year_min=2005,pop_group="Geschlecht - Total")
+
+function_inla_total(varBirth="single_birth",varPop="total_birth",Year_max=2022, Year_min=1987,pop_group="Geschlecht - Total")
+function_inla_total(varBirth="multiple_birth",varPop="total_birth",Year_max=2022, Year_min=1987,pop_group="Geschlecht - Total")
+
+function_inla_total(varBirth="females",varPop="total_birth",Year_max=2022, Year_min=1987,pop_group="Geschlecht - Total")
+function_inla_total(varBirth="males",varPop="total_birth",Year_max=2022, Year_min=1987,pop_group="Geschlecht - Total")
 
 
-
-
-function_inla_total(Year_max=2022, Year_min=1987,Sex="Geschlecht - Total")
-
-
-function_inla_total(Year_max=2022, Year_min=1987, Sex="Frau")
+function_inla_total(Year_max=2022, Year_min=1870, pop_group="Frau")
 
 
 
